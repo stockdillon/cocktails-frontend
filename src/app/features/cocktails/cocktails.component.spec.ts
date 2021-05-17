a8826f9882bb2574d9fb249bd428e7dd3e4ef984
Dillon Stock <stockdillon@yahoo.com>
2021-05-16 20:46:37 -0400

    cocktails tests

--- src/app/features/cocktails/cocktails.component.spec.ts
+++ src/app/features/cocktails/cocktails.component.spec.ts
@@ -1,9 +1,11 @@
+import { QueryType } from '../../shared';
 import { mockRoute } from './../../testing/mocks/route.mock';
 import { mockRouter } from './../../testing/mocks/router.mock';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 
 import { CocktailsComponent } from './cocktails.component';
+import { CockailIdentifier } from './models/query-indentifiers.const';
 
 describe('CocktailsComponent', () => {
   let component: CocktailsComponent;
@@ -29,4 +31,37 @@ describe('CocktailsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  describe('submit', () => {
+    it('should call router.navigate', () => {
+      const navigateSpy = spyOn(mockRouter, 'navigate').and.stub();
+      const mockQuery = {
+        displayText: '',
+        identifier: CockailIdentifier.Name,
+        query: 'vodka',
+        type: QueryType.Search
+      };
+      const mockExtras: NavigationExtras = {
+        relativeTo: undefined,
+      };      
+      component.query = mockQuery;
+      component.submit();
+      expect(navigateSpy).toHaveBeenCalled();
+      expect(navigateSpy).toHaveBeenCalledTimes(1);
+      expect(navigateSpy).toHaveBeenCalledWith(['cocktails', QueryType.Search, mockQuery.query], mockExtras);
+    });
+  })  
+
+  describe('applyFilter', () => {
+    it('should update the filter string of the datasource', () => {
+      const mockEvent = {
+        target: {
+          value: 'TEST'
+        }
+      };
+      component.applyFilter(mockEvent as any as Event);
+      expect(component.dataSource.filter).toEqual('test');
+    });
+  })    
 });

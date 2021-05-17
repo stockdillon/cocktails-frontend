791dea8c2a67f86d1f8a87e11eff1c77d8783f7f
Dillon Stock <stockdillon@yahoo.com>
2021-05-16 20:33:54 -0400

    test ingredient submit

--- src/app/features/ingredients/ingredients.component.spec.ts
+++ src/app/features/ingredients/ingredients.component.spec.ts
@@ -1,10 +1,12 @@
 import { mockRouter } from './../../testing/mocks/router.mock';
 import { mockRoute } from './../../testing/mocks/route.mock';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IngredientsComponent } from './ingredients.component';
 import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { IngredientIdentifier } from './models/query-indentifiers.const';
+import { QueryType } from '../../shared';
 
 describe('IngredientsComponent', () => {
   let component: IngredientsComponent;
@@ -34,4 +36,24 @@ describe('IngredientsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('submit', () => {
+    it('should call router.navigate', () => {
+      const navigateSpy = spyOn(mockRouter, 'navigate').and.stub();
+      const mockQuery = {
+        displayText: '',
+        identifier: IngredientIdentifier.Name,
+        query: 'vodka',
+        type: QueryType.Search
+      };
+      const mockExtras: NavigationExtras = {
+        relativeTo: undefined,
+      };      
+      component.query = mockQuery;
+      component.submit();
+      expect(navigateSpy).toHaveBeenCalled();
+      expect(navigateSpy).toHaveBeenCalledTimes(1);
+      expect(navigateSpy).toHaveBeenCalledWith(['ingredients', QueryType.Search, mockQuery.query], mockExtras);
+    });
+  })
 });
